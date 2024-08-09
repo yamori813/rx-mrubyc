b8bf5ce00a0d645cca6b79a154b5256b97066aa3
Hiroki Mori <yamori813@yahoo.co.jp>
2024-08-10 07:32:50 +0900

    use monitor.c

--- RX210/main.c
+++ RX210/main.c
@@ -15,22 +15,7 @@
 
 #include "xprintf.h"
 
-#define XMODEM_SOH	0x01
-#define XMODEM_STX	0x02
-#define XMODEM_EOT	0x04
-#define XMODEM_ACK	0x06
-#define XMODEM_NAK	0x15
-#define XMODEM_CAN	0x18
-#define XMODEM_EOF	0x1a /* Ctrl-Z */
-
 #define E2_BASE_ADDR	(0x00100000)
-
-#define MEMORY_SIZE (1024*10)
-static char memory_pool[MEMORY_SIZE];
-
-static void c_sci5_init (mrb_vm * vm, mrb_value * v);
-static void c_sci5_read (mrb_vm * vm, mrb_value * v);
-static void c_sci5_write (mrb_vm * vm, mrb_value * v);
 
 int statcon()
 {
@@ -65,11 +50,11 @@ void pchar(unsigned char ch)
 	SCI1.TDR = ch;
 }
 
-static void c_sci5_init (mrb_vm * vm, mrb_value * v)
+void c_sci5_init (mrb_vm * vm, mrb_value * v)
 {
 }
 
-static void c_sci5_read(mrb_vm * vm, mrb_value * v)
+void c_sci5_read(mrb_vm * vm, mrb_value * v)
 {
 	uint8_t ch;
 
@@ -77,7 +62,7 @@ static void c_sci5_read(mrb_vm * vm, mrb_value * v)
 	SET_INT_RETURN(ch);
 }
 
-static void c_sci5_write(mrb_vm * vm, mrb_value * v)
+void c_sci5_write(mrb_vm * vm, mrb_value * v)
 {
 	uint8_t ch = GET_INT_ARG(1);
 
@@ -92,14 +77,6 @@ int hal_write(int fd, const void * buf, int nbytes)
 int hal_flush(int fd)
 {
 	return 0;
-}
-
-mrbcopy(unsigned short *src, unsigned short *dst, int size)
-{
-int i;
-
-	for (i = 0; i < size / 2; ++i)
-		dst[i] = src[i];
 }
 
 /**
@@ -130,88 +107,5 @@ unsigned char ch;
 				break;
 		}
 	}
-}
-
-
-xrec()
-{
-unsigned char ch;
-unsigned char dat[128];
-int i, block;
-
-	block = 0;
-
-	xprintf("xmodem start after 10 secs\r\n", block);
-
-	EraseE2Flash(E2_BASE_ADDR, 128 * 64);
-
-	delay_ms(10000);
-	pchar(XMODEM_NAK);
-
-	while(1) {
-		ch = gchar();
-		if (ch == XMODEM_EOT) {
-			pchar(XMODEM_ACK);
-			break;
-		}
-		ch = gchar();
-		ch = gchar();
-		for(i = 0; i < 128; ++i)
-			dat[i] = gchar();
-		ch = gchar();
-
-		WriteE2Flash(E2_BASE_ADDR + block * 128, dat, 128);
-		++block;
-		pchar(XMODEM_ACK);
-	}
-
-	xprintf("%d blocks\r\n", block);
-
-}
-
-void runmrbc()
-{
-unsigned short *e2p;
-unsigned short ss;
-unsigned char ch;
-int i;
-int mrbsize;
-unsigned char *mrbbuf;
-
-	e2p = E2_BASE_ADDR;
-	for (i = 0; i < 8; ++i) {
-		ch = *(e2p + i);
-		xprintf("%02x ", ch);
-		ch = *(e2p + i) >> 8;
-		xprintf("%02x ", ch);
-	}
-	xprintf("\r\n");
-	ss = *(e2p + 4);
-	mrbsize = (ss & 0xff) << 24 | (ss >> 8) << 16;
-	ss = *(e2p + 5);
-	mrbsize += (ss & 0xff) << 8 | (ss >> 8);
-	mrbsize = ((mrbsize + 1) / 2) * 2;
-	xprintf("MRB SIZE %d\r\n", mrbsize);
-
-	mrbbuf = malloc(mrbsize);
-
-	mrbcopy(e2p, mrbbuf, mrbsize);
-
-	mrbc_init(memory_pool, MEMORY_SIZE);
-	mrbc_define_method(0, mrbc_class_object, "serial_init",
-	    (mrbc_func_t)c_sci5_init);
-	mrbc_define_method(0, mrbc_class_object, "serial_read",
-	    (mrbc_func_t)c_sci5_read);
-	mrbc_define_method(0, mrbc_class_object, "serial_write",
-	    (mrbc_func_t)c_sci5_write);
-
-	if (NULL == mrbc_create_task(mrbbuf, 0))
-	{
-		while(1);
-	}
-
-	mrbc_run();
-
-	return 0;
 }
 
