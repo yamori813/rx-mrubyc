23707c8c68dbbbb6645b1e2382d522b7580b3695
Hiroki Mori <yamori813@yahoo.co.jp>
2024-08-16 18:07:34 +0900

    gmake and bsd make support

--- mrubyc.mk
+++ mrubyc.mk
@@ -4,21 +4,40 @@ CROSS_CFLAGS += -std=c99 -I. -I../mrubyc/src
 CROSS_CFLAGS += -DMRBC_USE_STRING=1
 
 alloc.o : ../mrubyc/src/alloc.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 c_array.o : ../mrubyc/src/c_array.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 c_hash.o : ../mrubyc/src/c_hash.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 c_math.o : ../mrubyc/src/c_math.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 c_numeric.o : ../mrubyc/src/c_numeric.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 c_object.o : ../mrubyc/src/c_object.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 c_range.o : ../mrubyc/src/c_range.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 c_string.o : ../mrubyc/src/c_string.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 class.o : ../mrubyc/src/class.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 console.o : ../mrubyc/src/console.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 error.o : ../mrubyc/src/error.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 global.o : ../mrubyc/src/global.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 keyvalue.o : ../mrubyc/src/keyvalue.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 load.o : ../mrubyc/src/load.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 mrblib.o : ../mrubyc/src/mrblib.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 rrt0.o : ../mrubyc/src/rrt0.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 symbol.o : ../mrubyc/src/symbol.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 value.o : ../mrubyc/src/value.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
 vm.o : ../mrubyc/src/vm.c
+	$(CROSS_CC) $(CROSS_CFLAGS) -c $<
