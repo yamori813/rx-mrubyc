fcc97d911b37dab669a13be1fd5fd4f503e878a6
Hiroki Mori <yamori813@yahoo.co.jp>
2024-08-24 21:52:28 +0900

    add script

---
+++ samples/sleep.rb
@@ -0,0 +1,18 @@
+# built in method sleep test
+
+def print(val)
+  val.to_s.each_char do |n|
+    serial_write(n.ord)
+  end
+end
+
+begin
+  serial_init
+
+  loop do
+    serial_write(0x2a)
+    sleep 1
+  end
+rescue StandardError => e
+  print e
+end
